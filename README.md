9e1a9283209a5bba71d48ba296ba0131a9fc734a
Silas Stoffel <silasstofel@gmail.com>
2025-04-03 22:55:56 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Go-gRPC
+
+## Setup
+
+```
+docker-compose up -d
+
+# accessing the container
+./scripts/golang-app.sh
+
+```
---
+++ scripts/golang-app.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo ""
+echo "You're running Go-gRPC on the docker container:"
+echo ""
+
+docker exec -it golang-app bash
+
+#docker-compose down
